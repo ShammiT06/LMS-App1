d843245f2da9f5300d34ba466077642c72e30dda
VishnupriyaR06 <vishnupriya3rdviz@gmail.com>
2025-12-22 12:21:44 +0530

    push

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text , Image} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import ProfileStyles from '../../assets/Styles/tabs-style/Profile'
 
 export default function Profile() {
+
   return (
+    <SafeAreaView>
     <View>
-      <Text>Profile</Text>
+      <Text style={ProfileStyles.container}>My Profile</Text>
+      {/* <Image src='img'/> */}
+      <View></View>
     </View>
+    </SafeAreaView>
   )
 }
---
+++ assets/Styles/tabs-style/Profile.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const ProfileStyles = StyleSheet.create({
+  container:{
+    flex:1,
+     color:'#FF6B2C',
+     font:"semibold",
+     justifyContent:"center",
+  }
+})
+
+
+export default ProfileStyles
