24b4623705e052396706f742b1bb422e00d43c5e
VishnupriyaR06 <vishnupriya3rdviz@gmail.com>
2025-12-22 14:26:55 +0530

    pushh

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,16 +1,44 @@
-import { View, Text , Image} from 'react-native'
-import React from 'react'
+import { View, Text ,Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ProfileStyles from '../../assets/Styles/tabs-style/Profile'
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Profile() {
-
   return (
-    <SafeAreaView>
-    <View>
-      <Text style={ProfileStyles.container}>My Profile</Text>
-      {/* <Image src='img'/> */}
-      <View></View>
+    <SafeAreaView style={ProfileStyles.safe}>
+    <View style={ProfileStyles.container}>
+      <Text style={ProfileStyles.containerTxt}>
+         My Profile
+      </Text>  
+      <Image
+       source={require('../../assets/images/profile.png')}
+       style={ProfileStyles.profileImg} />
+      <View style={ProfileStyles.content}> 
+         <View style={ProfileStyles.contentTxt}>
+            <View style={ProfileStyles.iconBg}>
+           <Ionicons name="person" style={ProfileStyles.ionicons} />
+          </View>
+        <Text> Profile</Text>   
+         </View> 
+          <View style={ProfileStyles.contentTxt}>
+            <View style={ProfileStyles.iconBg}>
+             <Ionicons name="language" style={ProfileStyles.ionicons} />
+         </View>
+          <Text> Language</Text>  
+         </View> 
+          <View style={ProfileStyles.contentTxt}>
+            <View style={ProfileStyles.iconBg}>
+             <Ionicons name="wallet-outline" style={ProfileStyles.ionicons} />
+          </View>
+           <Text> Theme</Text>
+         </View> 
+          <View style={ProfileStyles.contentTxt}>
+            <View style={ProfileStyles.iconBg}>
+              <Ionicons name="log-out-outline" size={24} color="black"/>
+            </View>
+            <Text> Logout</Text>
+         </View> 
+      </View>
     </View>
     </SafeAreaView>
   )
--- assets/Styles/tabs-style/Profile.js
+++ assets/Styles/tabs-style/Profile.js
@@ -1,12 +1,50 @@
 import { StyleSheet } from "react-native";
 
 const ProfileStyles = StyleSheet.create({
-  container:{
-    flex:1,
-     color:'#FF6B2C',
-     font:"semibold",
-     justifyContent:"center",
-  }
+    safe: {
+        flex:1
+    },
+    container:{
+        marginTop:20,
+    },
+   containerTxt: {
+  textAlign: "center",
+  justifyContent:"center",
+  alignItems:"center",
+  color: "#FF6B2C",
+  width: 110,
+  height: 30,
+  fontSize: 20,
+  fontWeight: "600", // semibold equivalent
+},
+content:{
+    padding:20,
+},
+contentTxt:{
+    flexDirection:"row",
+    alignItems:"center",
+    gap:10,
+    paddingTop:10,
+    fontSize:15,
+},
+iconBg:{
+  borderWidth: 1,
+  borderColor: "#000",
+  padding: 10,
+  borderRadius: 50,
+  alignItems: "center",
+  justifyContent: "center"
+},
+profileImg: {
+  width: 80,
+  height: 80,
+  borderRadius: 40,
+  resizeMode: 'cover',
+},
+ionicons:{
+    borderColor:'#000000',
+    
+},
 })
 
 
