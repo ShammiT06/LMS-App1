33e2d56aeab6dfbe3ae226706dc6526359bf79c1
ShammiT06 <shammikumar.fullstack@gmail.com>
2025-12-22 12:23:01 +0530

    updated

--- app/(guest)/index.jsx
+++ app/(guest)/index.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Home</Text>
+        <View>
+        <Text>Hi, There</Text>
+        <Text>Let's start Learning !</Text>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   )
